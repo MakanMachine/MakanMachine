5dd44b69ffc4ae70d7c46a3d19d34841afa1a22e
Javen <javenseow@gmail.com>
2018-05-28 21:16:01 +0800

    update to webhook router function

--- routes/webhook.js
+++ routes/webhook.js
@@ -4,7 +4,7 @@ var tgCaller = require('../api_caller/telegram_caller');
 var eventHandler = require('../handlers/eventHandler');
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
+router.post('/', function(req, res) {
 	const objectBody = req.body;
 	console.log('received message from telegram', JSON.stringify(objectBody));
 	res.status(200).send('Rubbish');
