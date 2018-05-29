a29940c39cc3439c3da22972777289c25f887b28
Kok Yuan <warfear@hotmail.com>
2018-05-30 01:33:28 +0800

    successful mongoose

--- userpref.js
+++ userpref.js
@@ -1,2 +1,29 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://<makan_machine>:<!H4rdM@kan>@ds235860.mlab.com:35860/mm_user_pref');
+const express = require('express');
+
+mongoose.connect('mongodb://makan_machine:!H4rdM%40kan@ds235860.mlab.com:35860/mm_user_pref');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error.'));
+
+db.once('open', function() {
+    console.log('Successfully connected.')
+
+    var userSchema = mongoose.Schema({
+        username: String,
+        first_name: String,
+        cuisine: String,
+        created_at: Date,
+        updated_at: Date
+    });
+
+    var User = mongoose.model('User', userSchema, 'User');
+
+    User.create({
+        username: 'test',
+        first_name: 'te',
+        cuisine: "test cuisine",
+        created_at: new Date(),
+        updated_at: new Date()
+    });
+});
