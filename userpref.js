40947b3ce45d4ce733ff0fa291500bd89b8d60a9
Kok Yuan <warfear@hotmail.com>
2018-05-31 02:10:33 +0800

    edit userpref

--- userpref.js
+++ userpref.js
@@ -7,23 +7,49 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error.'));
 
 db.once('open', function() {
-    console.log('Successfully connected.')
+    console.log('Successfully connected.');
 
     var userSchema = mongoose.Schema({
-        username: String,
-        first_name: String,
-        cuisine: String,
-        created_at: Date,
-        updated_at: Date
+    username: String,
+    chatid: String,
+    first_name: String,
+    cuisine: String,
+    created_at: Date,
+    updated_at: Date
     });
 
     var User = mongoose.model('User', userSchema, 'User');
 
+});
+
+function createNewUser(username, chatid, first_name, cuisine)
     User.create({
-        username: 'test',
-        first_name: 'te',
-        cuisine: "test cuisine",
+        username: username,
+        chatid: chatid,
+        first_name: first_name,
+        cuisine: cuisine,
         created_at: new Date(),
         updated_at: new Date()
     });
-});
+
+// db.once('open', function() {
+//     console.log('Successfully connected.')
+
+//     var userSchema = mongoose.Schema({
+//         username: String,
+//         first_name: String,
+//         cuisine: String,
+//         created_at: Date,
+//         updated_at: Date
+//     });
+
+//     var User = mongoose.model('User', userSchema, 'User');
+
+//     User.create({
+//         username: 'test',
+//         first_name: 'te',
+//         cuisine: "test cuisine",
+//         created_at: new Date(),
+//         updated_at: new Date()
+//     });
+// });
