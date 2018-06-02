1d2474d9bc0e67ce029a7fcb25bbde93c961f472
Kok Yuan <warfear@hotmail.com>
2018-06-02 14:57:58 +0800

    added start command to userpref

--- userpref.js
+++ userpref.js
@@ -1,28 +1,29 @@
 const mongoose = require('mongoose');
 const express = require('express');
 
-mongoose.connect('mongodb://makan_machine:!H4rdM%40kan@ds235860.mlab.com:35860/mm_user_pref');
+exports.start = function() {
+    mongoose.connect('mongodb://makan_machine:!H4rdM%40kan@ds235860.mlab.com:35860/mm_user_pref');
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection error.'));
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Connection error.'));
 
-db.once('open', function() {
-    console.log('Successfully connected.');
+    db.once('open', function() {
+        console.log('Successfully connected.');
 
-    var userSchema = mongoose.Schema({
-    username: String,
-    chatid: String,
-    first_name: String,
-    cuisine: String,
-    created_at: Date,
-    updated_at: Date
+        var userSchema = mongoose.Schema({
+        username: String,
+        chatid: String,
+        first_name: String,
+        cuisine: String,
+        created_at: Date,
+        updated_at: Date
+        });
+
+        var User = mongoose.model('User', userSchema, 'User');
     });
+}
 
-    var User = mongoose.model('User', userSchema, 'User');
-
-});
-
-function createNewUser(username, chatid, first_name, cuisine)
+function createNewUser(username, chatid, first_name, cuisine) {
     User.create({
         username: username,
         chatid: chatid,
@@ -31,6 +32,7 @@ function createNewUser(username, chatid, first_name, cuisine)
         created_at: new Date(),
         updated_at: new Date()
     });
+}
 
 // db.once('open', function() {
 //     console.log('Successfully connected.')
