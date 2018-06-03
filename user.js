866e0eb051b6241a697003641084f17439033c43
Kok Yuan <warfear@hotmail.com>
2018-06-03 11:23:08 +0800

    working on mlab

---
+++ user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+username: String,
+chat_id: String,
+first_name: String,
+cuisine: String,
+created_at: Date,
+updated_at: Date
+});
+
+var User = mongoose.model('User', userSchema, 'User');
+
+module.exports = User;
--- userpref.js
+++ userpref.js
@@ -1,38 +1,34 @@
-const mongoose = require('mongoose');
-const express = require('express');
+var mongoose = require('mongoose');
+var express = require('express');
+var User = require('./user');
 
-exports.start = function() {
-    mongoose.connect('mongodb://makan_machine:!H4rdM%40kan@ds235860.mlab.com:35860/mm_user_pref');
+mongoose.connect('mongodb://makan_machine:!H4rdM%40kan@ds235860.mlab.com:35860/mm_user_pref');
 
-    var db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'Connection error.'));
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error.'));
 
-    db.once('open', function() {
-        console.log('Successfully connected.');
+db.once('open', function() {
+    console.log('Successfully connected.');
+});
 
-        var userSchema = mongoose.Schema({
-        username: String,
-        chatid: String,
-        first_name: String,
-        cuisine: String,
-        created_at: Date,
-        updated_at: Date
-        });
-
-        var User = mongoose.model('User', userSchema, 'User');
-    });
+function handleUser(objectBody) {
+    User.findOneAndUpdate({chat_id: objectBody.message.chat.id}, )
 }
 
-function createNewUser(username, chatid, first_name, cuisine) {
+function createNewUser(username, chatID, first_name, cuisine) {
     User.create({
         username: username,
-        chatid: chatid,
+        chat_id: chatid,
         first_name: first_name,
         cuisine: cuisine,
         created_at: new Date(),
         updated_at: new Date()
     });
 }
+
+module.exports = {
+    createNewUser
+};
 
 // db.once('open', function() {
 //     console.log('Successfully connected.')
