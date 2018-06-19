503b0e9d730e39d7e6ca2d7fb80746944c76f18f
Kok Yuan <warfear@hotmail.com>
2018-06-19 15:41:02 +0800

    changed func name in cacheservice

--- cache/cacheService.js
+++ cache/cacheService.js
@@ -3,12 +3,13 @@ const cacheProvider = require('./cacheProvider');
 
 // Takes in an options object containing cuisine and location.
 function get(options) {
+    console.log(`Getting cache value for cuisine: ${options.cuisine}.`)
     return new Promise((resolve, reject) => {
         cacheProvider.getInstance()[cuisine].get(options.cuisine, (err, result) => {
             if(err) {
                 reject(new Error(`Unable to get cache value for cuisine: ${options.cuisine} => Err: ${err}`));
             } else {
-                console.log(`Got cache value for cuisine: ${options.cuisine}`);
+                console.log(`Got cache value for cuisine: ${options.cuisine}.`);
                 resolve(result);
             }
         });
