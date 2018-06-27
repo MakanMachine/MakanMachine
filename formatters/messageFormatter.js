aafd98e4a70c6555cb3386ca0f9915305d843f57
Javen <javenseow@gmail.com>
2018-06-27 14:15:54 +0800

    updated new line

--- formatters/messageFormatter.js
+++ formatters/messageFormatter.js
@@ -6,7 +6,8 @@ function formatRestaurantMessage(array) {
 Address: ${x.address}
 Opening hours: ${x.opening_hours}
 Nearest MRT: ${x.nearest_mrt}
-Google Maps: ${x.map_url}\n`;
+Google Maps: ${x.map_url}
+\n`;
         newArr.push(message);
     }
     console.log(newArr);
