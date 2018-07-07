08dc464d21c81753191590e94253785d39f0e65a
Kok Yuan <warfear@hotmail.com>
2018-07-07 17:12:52 +0800

    fix

--- location/locationService.js
+++ location/locationService.js
@@ -1,15 +1,15 @@
 const Geo = require('geo-nearby');
-const cacheService = require('../cache/cacheService');
+const cService = require('../cache/cacheService');
 
 async function filterLocation(content, long, lati){
     console.log(`Filtering by long: ${long}, lati: ${lati}.`);
     const dataSet = Geo.createCompactSet(content, {id: '_id', lat: 'lat', lon: 'lng'});
     console.log(`dataSet: ${dataSet}`);
     const geo = new Geo(dataSet, {sorted: true});
-    const nearby = geo.nearBy(lati, long, 1500);
+    const nearby = geo.nearBy(lati, long, 15000);
     var arr = [];
     for(var x of nearby) {
-        value = await cacheService.get(cacheService.cacheTables.ID, x["i"]);
+        value = await cService.get(cService.cacheTables.ID, x["i"]);
         arr.push(value);
         console.log(value);
     }
