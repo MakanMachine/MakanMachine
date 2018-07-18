feefdd7d237234d8778bb09be2cfbd483c3d6b72
Javen <javenseow@gmail.com>
2018-07-18 23:24:34 +0800

    changed location distance to 5km

--- services/locationService.js
+++ services/locationService.js
@@ -7,7 +7,7 @@ async function filterLocation(content, long, lati){
     const dataSet = Geo.createCompactSet(content, {id: '_id', lat: 'lat', lon: 'lng'});
     console.log(`dataSet: ${dataSet}`);
     const geo = new Geo(dataSet, {sorted: true});
-    const nearby = geo.nearBy(lati, long, 7000);
+    const nearby = geo.nearBy(lati, long, 5000);
     return nearby;
 }
 
