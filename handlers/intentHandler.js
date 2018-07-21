50f8f1e89842b8e3bd7f786249ddaeedc7b55513
Kok Yuan <warfear@hotmail.com>
2018-07-22 02:25:26 +0800

    fix

--- handlers/intentHandler.js
+++ handlers/intentHandler.js
@@ -17,12 +17,12 @@ async function handleIntent(chatID, dfQueryResult, userQuery, msgObj) {
                 
                 case 'recommend_cuisine':
                 await tgCaller.sendMessage(chatID, dfQueryResult.fulfillmentText);
-                await rHandler.handleReplyIntent(chatID, msgObj.chat.first_name, {text: dfQueryResult.parameters.cuisine, type: 'recommend'});
+                await rHandler.handleReplyIntent(chatID, msgObj.chat.first_name, {text: dfQueryResult.parameters.fields.cuisine.stringValue, type: 'recommend'});
                 break;
 
                 case 'recommend_cuisine_location':
                 await tgCaller.sendMessage(chatID, dfQueryResult.fulfillmentText);
-                await rHandler.handleReplyIntent(chatID, msgObj.chat.first_name, {text: dfQueryResult.parameters.cuisine, type: 'recommend'});
+                await rHandler.handleReplyIntent(chatID, msgObj.chat.first_name, {text: dfQueryResult.parameters.fields.cuisine.stringValue, type: 'recommend'});
                 break;
 
                 case 'surprise_me':
