71afa8e90bfede30eff6c7211bad98f3fca4d4cd
Kok Yuan <warfear@hotmail.com>
2018-07-22 22:23:39 +0800

    edit filterloc

--- services/locationService.js
+++ services/locationService.js
@@ -4,6 +4,9 @@ const Geo = require('geo-nearby');
 // Returns filtered array of restaurant IDs.
 async function filterLocation(content, long, lati){
     console.log(`Filtering by long: ${long}, lati: ${lati}.`);
+    if(content == undefined) {
+        return [];
+    }
     const dataSet = Geo.createCompactSet(content, {id: '_id', lat: 'lat', lon: 'lng'});
     console.log(`dataSet: ${dataSet}`);
     const geo = new Geo(dataSet, {sorted: true});
