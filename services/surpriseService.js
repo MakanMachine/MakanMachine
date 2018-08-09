8db0bb034a1223852873ea6f6cd384fddc006984
Kok Yuan <warfear@hotmail.com>
2018-08-09 20:22:01 +0800

    update surprise_me to display cuisine

--- services/surpriseService.js
+++ services/surpriseService.js
@@ -39,9 +39,9 @@ async function surprise(options) {
         var result = arrCuisine[Math.floor(Math.random() * arrCuisine.length)];
 
         if(result) {
-        console.log(`User: ${user}.`);
-        console.log(`Result: ${result.name}`);
-        message = `*${result.name}*\n\nAddress: ${result.address}\nOpening hours:\n${result.opening_hours}\nNearest MRT: ${result.nearest_mrt}\nGoogle Maps: [View Map](${result.map_url})`;
+            console.log(`User: ${user}.`);
+            console.log(`Result: ${result.name}`);
+            message = `*${result.name}*\n[${result.cuisine.toString()}\n\nAddress: ${result.address}\nOpening hours:\n${result.opening_hours}\nNearest MRT: ${result.nearest_mrt}\nGoogle Maps: [View Map](${result.map_url})`;
         } else {
             message = `Oops! We could not find you a restaurant based on your preferences. Run /settings to edit your preferences!`;
         }
