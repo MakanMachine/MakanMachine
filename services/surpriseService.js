a8d45c6fd37047e9b01ef313f24b840c17ef465c
Kok Yuan <warfear@hotmail.com>
2018-08-09 20:41:57 +0800

    changed cuisine to italic

--- services/surpriseService.js
+++ services/surpriseService.js
@@ -41,7 +41,7 @@ async function surprise(options) {
         if(result) {
             console.log(`User: ${user}.`);
             console.log(`Result: ${result.name}`);
-            message = `*${result.name}*\n\x5B${result.cuisine.toString()}\x5D\n\nAddress: ${result.address}\nOpening hours:\n${result.opening_hours}\nNearest MRT: ${result.nearest_mrt}\nGoogle Maps: [View Map](${result.map_url})`;
+            message = `*${result.name}*\n_${result.cuisine.toString()}_\n\nAddress: ${result.address}\nOpening hours:\n${result.opening_hours}\nNearest MRT: ${result.nearest_mrt}\nGoogle Maps: [View Map](${result.map_url})`;
         } else {
             message = `Oops! We could not find you a restaurant based on your preferences. Run /settings to edit your preferences!`;
         }
