f90fd69e02dc7821f848e580eb5114f232900085
Kok Yuan <warfear@hotmail.com>
2018-08-19 16:12:06 +0800

    set location to 2km

--- services/locationService.js
+++ services/locationService.js
@@ -10,7 +10,7 @@ async function filterLocation(content, long, lati) {
     const dataSet = Geo.createCompactSet(content, {id: '_id', lat: 'lat', lon: 'lng'});
     console.log(`dataSet: ${dataSet}`);
     const geo = new Geo(dataSet, {sorted: true});
-    const nearby = geo.nearBy(lati, long, 5000);
+    const nearby = geo.nearBy(lati, long, 2000);
     return nearby;
 }
 
